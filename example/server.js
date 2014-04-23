7d9d2d7f0c8614cece39f82a0cc13ce34c816522
Brian Mulloy <brian@landlessness.net>
2014-04-23 16:49:41 -0400

    fixed package typo. removed sample app.

--- example/server.js
+++ example/server.js
@@ -1,10 +1,8 @@
 var zetta = require('zetta');
 var SerialDevice = require('zetta-serial-device-driver');
 var FonaSMS = require('../index');
-var app = require('./app');
 
 zetta()
   .use(SerialDevice, '/dev/ttyO1')
   .use(FonaSMS)
-  .use(app)
   .listen(1337);
