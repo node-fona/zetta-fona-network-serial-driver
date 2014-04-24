b16e95a51154693a41c4db39ea5505d71a886103
Brian Mulloy <brian@landlessness.net>
2014-04-23 20:25:00 -0400

    added delete sms

--- fona_sms.js
+++ fona_sms.js
@@ -5,7 +5,7 @@ var FonaSMS = module.exports = function() {
   Device.call(this);
   this._serialDevice = arguments[0];
 
-  this.smsCount = null;
+  this.smsMaxIndex = null;
   this.smsMessages = {};
 };
 util.inherits(FonaSMS, Device);
@@ -13,24 +13,32 @@ util.inherits(FonaSMS, Device);
 FonaSMS.prototype.init = function(config) {
 
   config
-  .name('Adafruit FonaSMS')
+  .name('Adafruit Fona SMS')
   .type('fona-sms')
-  .monitor('smsCount')
+  .monitor('smsMaxIndex')
   .state('waiting')
   .when('waiting', { allow: ['send-sms', 'read-sms', 'delete-sms']})
   .when('sending-sms', { allow: ['read-sms','delete-sms']})
   .when('reading-sms', { allow: ['send-sms']})
   .when('deleting-sms', { allow: ['send-sms']})
-  .map('delete-sms', this.deleteSMS, [
-    { name: 'messageIndex', title: 'Message Index', type: 'range',
-      min: 1, step: 1}])
-  .map('read-sms', this.readSMS, [
-    { name: 'messageIndex', title: 'Message Index', type: 'range',
-      min: 1, step: 1}])
   .map('send-sms', this.sendSMS, [
     { name: 'phoneNumber', title: 'Phone Number to Send SMS', type: 'text'},
     { name: 'message', title: 'Body of the SMS', type: 'text'},
-    ]);
+    ])
+  .map('read-sms', this.readSMS, [
+    { name: 'messageIndex', title: 'Message Index', type: 'range',
+      min: 1, max: 80, step: 1}])
+  .map('delete-sms', this.deleteSMS, [
+    { name: 'messageIndex', title: 'Message Index', type: 'range',
+      min: 1, max: 80, step: 1},
+    { name: 'flag', title: 'Options Flag', type: 'range',
+      min: 0, max: 4, step: 1, value: 0, notes: {
+        0: 'Delete only the SMS message stored at the location index from the message storage area. This is the default value.',
+        1: 'Ignore the value of index and delete all SMS messages whose status is "received read" from the message storage area.',
+        2: 'Ignore the value of index and delete all SMS messages whose status is "received read" or "stored sent" from the message storage area.',
+        3: 'Ignore the value of index and delete all SMS messages whose status is "received read", "stored unsent" or "stored sent" from the message storage area.',
+        4: 'Ignore the value of index and delete all SMS messages from the message storage area.'
+      }}]);
 
   var self = this;
   this._requestAllSMSMessages();
@@ -82,30 +90,49 @@ FonaSMS.prototype.readSMS = function(messageIndex, cb) {
     name: 'Read SMS: ' + messageIndex,
     command: 'AT+CSDH=1', 
     regexps: [/^AT\+CSDH=1/,/OK/]}, function() {});
-  this._serialDevice.enqueue({ 
+  this._serialDevice.enqueue({
     name: 'Read SMS: ' + messageIndex,
     command: 'AT+CMGR=' + messageIndex, 
     regexps: [/^AT\+CMGR=\d+/,
-      /^OK|\+CMGR: "([A-Z]+) ([A-Z]+)","([^"]*)","([^"]*)","([^"]*)",(\d+),(\d+),(\d+),(\d+),"([^"]*)",(\d+),(\d+)/,
-      /^|(.*)$/,
-      /^$/,
-      /^|OK$/]},
-    function (matches) {
-      self.smsMessages[messageIndex] = {
-        receivedState: matches[1][1],
-        readState: matches[1][2],
-        sendersPhoneNumber: matches[1][3],
-        timeStamp: matches[1][5],
-        body: matches[2][0]
-      };
-      self.state = 'waiting';
-      cb();
-    });
+      /^(OK)|\+CMGR: "([A-Z]+) ([A-Z]+)","([^"]*)","([^"]*)","([^"]*)",(\d+),(\d+),(\d+),(\d+),"([^"]*)",(\d+),(\d+)/]}, function (matches) {
+      if (matches[1][1] === 'OK') {
+        self.state = 'waiting';
+        cb();
+      } else {
+        self.smsMessages[messageIndex] = {
+          receivedState: matches[1][1],
+          readState: matches[1][2],
+          sendersPhoneNumber: matches[1][3],
+          timeStamp: matches[1][5],
+        };
+        self._serialDevice.enqueue({
+            priority: self._serialDevice.highPriority,
+            command: null,
+            regexps: [/^(.*)$/,/^$/,/^OK$/]
+          }, function(matches) {
+            self.smsMessages[messageIndex] = {
+              body: matches[1][0]
+            };
+            self.state = 'waiting';
+            cb();
+          });
+    }
+  });
 }
 
-FonaSMS.prototype.deleteSMS = function(messageIndex, cb) {
+FonaSMS.prototype.deleteSMS = function(messageIndex, flag, cb) {
+  var deleteCommand = 'AT+CMGD=';
+
+  if (flag) {
+    // if flag then messageIndex param is ignored
+    deleteCommand += '1,' + flag;
+  } else {
+    deleteCommand += messageIndex;
+  }
   
   this.state = 'deleting-sms';
+  
+  console.log('messageIndex:',messageIndex);
   
   this.log('deleteSMS: ' + messageIndex);
   var self = this;
@@ -114,9 +141,10 @@ FonaSMS.prototype.deleteSMS = function(messageIndex, cb) {
     command: 'AT+CMGF=1',
     regexps: [/^AT\+CMGF=1/,/OK/]}, function() {});
   this._serialDevice.enqueue({
-    command: 'AT+CMGD=' + messageIndex, 
-    regexps: [/^AT\+CMGD=\d+/,/OK/]}, function() {
-      self.smsMessages[messageIndex] = {};
+    command: deleteCommand,
+    regexps: [/^AT\+CMGD=\d+[,\d+]+/,/OK/]}, function() {
+      // TODO consider updating smsMessages with deletions
+      // including deleting more based on flag
       self.state = 'waiting';
       cb();
     });
@@ -125,19 +153,19 @@ FonaSMS.prototype.deleteSMS = function(messageIndex, cb) {
 FonaSMS.prototype._requestAllSMSMessages = function() {
   this.log('Request ALL SMS Messages');
   var self = this;
-  this._requestSMSCountAndCapacity(function() {
-    for (messageIndex = 1; messageIndex <= self.smsCount; messageIndex++) {
+  this._requestsmsMaxIndexAndCapacity(function() {
+    for (messageIndex = 1; messageIndex <= self.smsMaxIndex; messageIndex++) {
       self.readSMS(messageIndex, function() {});
     }});
 }
 
-FonaSMS.prototype._requestSMSCountAndCapacity = function(cb) {
+FonaSMS.prototype._requestsmsMaxIndexAndCapacity = function(cb) {
   var self = this;
   this._serialDevice.enqueue({
     command: 'AT+CMGF=1', 
     regexps: [/^AT\+CMGF=1/,/OK/]}, function() {});
   this._serialDevice.enqueueSimple('AT+CPMS?', /^\+CPMS: "[A-Z_]+",(\d+),(\d+),.*$/, function (matches) {
-    self.smsCount = matches[1][1];
+    self.smsMaxIndex = matches[1][1];
     self.smsCapacity = matches[1][2];
     cb();
   });
